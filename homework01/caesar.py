6374314df1fc7ef625d773406bb48af05ed3cd75
Вагер Марк Игоревич <71377953+mivager@users.noreply.github.com>
2021-01-25 21:34:31 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,19 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for letter in plaintext:
+        if "A" <= letter <= "Z" or "a" <= letter <= "z":
+            if "A" <= letter <= "Z" and chr(ord(letter) + shift) > "Z":
+                letter = chr(ord(letter) - 26 + shift)
+                ciphertext += letter
+            elif "a" <= letter <= "z" and chr(ord(letter) + shift) > "z":
+                letter = chr(ord(letter) - 26 + shift)
+                ciphertext += letter
+            else:
+                letter = chr(ord(letter) + shift)
+                ciphertext += letter
+        else:
+            ciphertext += letter
     return ciphertext
 
 
@@ -33,7 +45,19 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for letter in ciphertext:
+        if "A" <= letter <= "Z" or "a" <= letter <= "z":
+            if "A" <= letter <= "Z" and chr(ord(letter) - shift) < "A":
+                letter = chr(ord(letter) + 26 - shift)
+                plaintext += letter
+            elif "a" <= letter <= "z" and chr(ord(letter) - shift) < "a":
+                letter = chr(ord(letter) + 26 - shift)
+                plaintext += letter
+            else:
+                letter = chr(ord(letter) - shift)
+                plaintext += letter
+        else:
+            plaintext += letter
     return plaintext
 
 
@@ -42,5 +66,13 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     Brute force breaking a Caesar cipher.
     """
     best_shift = 0
-    # PUT YOUR CODE HERE
+     for key in range(len(ciphertext)):
+        translated = ""
+        for symbol in dictionary:
+            if symbol in ciphertext:
+                num = ciphertext.find(symbol)
+                num -= key
+                if num < 0:
+                    num += len(ciphertext)
+                    translated += symbol 
     return best_shift
