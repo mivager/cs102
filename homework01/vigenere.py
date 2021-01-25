e1580702cf30ac762a458897c9bdec6ae72fc006
Вагер Марк Игоревич <71377953+mivager@users.noreply.github.com>
2021-01-25 21:46:54 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,19 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for l in range(len(plaintext)):
+        keyword = keyword.upper()
+        pos = l % len(keyword)
+        shift = ord(keyword[pos]) - ord("A")
+        if "a" <= plaintext[l] <= "z" or "A" <= plaintext[l] <= "Z":
+            if plaintext[l].isupper() and chr(ord(plaintext[l]) + shift) > "Z":
+                ciphertext += chr(ord(plaintext[l]) - 26 + shift)
+            elif "a" <= plaintext[l] <= "z" < chr(ord(plaintext[l]) + shift):
+                ciphertext += chr(ord(plaintext[l]) - 26 + shift)
+            else:
+                ciphertext += chr(ord(plaintext[l]) + shift)
+        else:
+            ciphertext += plaintext[l]
     return ciphertext
 
 
@@ -26,5 +38,17 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for l in range(len(ciphertext)):
+        keyword = keyword.upper()
+        pos = l % len(keyword)
+        shift = ord(keyword[pos]) - ord("A")
+        if "a" <= ciphertext[l] <= "z" or "A" <= ciphertext[l] <= "Z":
+            if ciphertext[l].isupper() and chr(ord(ciphertext[l]) - shift) < "A":
+                plaintext += chr(ord(ciphertext[l]) + 26 - shift)
+            elif chr(ord(ciphertext[l]) - shift) < "a" <= ciphertext[l] <= "z":
+                plaintext += chr(ord(ciphertext[l]) + 26 - shift)
+            else:
+                plaintext += chr(ord(ciphertext[l]) - shift)
+        else:
+            plaintext += ciphertext[l]
     return plaintext
